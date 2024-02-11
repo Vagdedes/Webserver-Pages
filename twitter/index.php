809654ad8b97b2d3afba2c2b72c1cd7c6dccf338
Evangelos <contact@vagdedes.com>
2024-02-11 11:41:31 +0200

    Added social media links

---
+++ twitter/index.php
@@ -0,0 +1,4 @@
+<?php
+require '/var/www/.structure/library/base/utilities.php';
+redirect_to_url("https://twitter.com/vagdedes");
+exit();
---
+++ youtube/index.php
@@ -0,0 +1,4 @@
+<?php
+require '/var/www/.structure/library/base/utilities.php';
+redirect_to_url("https://www.youtube.com/@Vagdedes");
+exit();
