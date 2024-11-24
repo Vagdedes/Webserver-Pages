8dd2dd16a6649459be0cd46ea4d0e41f6dd313ab
Evangelos <vaggdedes@gmail.com>
2024-11-24 23:39:33 +0200

    Update index.php

--- spartan/statistics/punishedPlayersCount/index.php
+++ spartan/statistics/punishedPlayersCount/index.php
@@ -2,6 +2,6 @@
 require_once '/var/www/.structure/library/base/communication.php';
 require '/var/www/.structure/library/gameCloud/init.php';
 echo sizeof(get_sql_query(
-    $punished_players_table,
+    GameCloudVariables::PUNISHED_PLAYERS_TABLE,
     array("id")
 ));
